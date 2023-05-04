73d2fe5df84fe33bd83c03633f9396e735d00d38
Olga Oblat <oa.oblat@gmail.com>
2023-05-04 23:12:35 +0300

    changed main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService ();
-        int amount = 1_000_000;
-        int numberOfYears = 1;
-        int total = service.calculate(amount, numberOfYears);
+        // int amount = 1_000_000;
+        // int numberOfYears = 1;
+        int total = service.calculate(1_000_000, 1);
         System.out.println(total);
     }
 }
