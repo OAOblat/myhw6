24b5d84489892a57e78552e5a051c9a808001bcc
Olga Oblat <oa.oblat@gmail.com>
2023-05-04 23:18:16 +0300

    changed main 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService ();
         // int amount = 1_000_000;
         // int numberOfYears = 1;
-        int total = service.calculate(1_000_000, 1);
-        System.out.println(total);
+        int totalOne = service.calculate(1_000_000, 1);
+        int totalTwo = service.calculate(1_000_000, 2);
+        int totalThree = service.calculate(1_000_000, 3);
+
+        System.out.println(totalOne);
+        System.out.println(totalTwo);
+        System.out.println(totalThree);
+
     }
 }
